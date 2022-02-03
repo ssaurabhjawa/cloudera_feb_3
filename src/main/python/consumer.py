4744e635e2a42356907120ed1be4684921dcd766
saurabhjawa <jai_dev@macs-Mac-mini.local>
2022-02-03 18:54:18 +0530

    consumer-group-id_1

--- src/main/python/consumer.py
+++ src/main/python/consumer.py
@@ -6,7 +6,8 @@ import getpass
 
 def kafka_consumer(env, appName):
     conf = {'bootstrap.servers': 'w01.itversity.com:9092,w02.itversity.com:9092',
-            'group.id': "foo"
+            'group.id': "foo",
+            'auto.offset.reset': 'smallest'
             }
     c = Consumer(conf)
 
