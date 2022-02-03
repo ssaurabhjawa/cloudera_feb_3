0e84c7d2894e220eb8a3968895b37feda7b3f750
saurabhjawa <jai_dev@macs-Mac-mini.local>
2022-02-03 16:54:27 +0530

    runeme

--- src/main/runme.sh
+++ src/main/runme.sh
@@ -2,4 +2,4 @@ export HADOOP_CONF_DIR="/etc/hadoop/conf"
 export PYSPARK_PYTHON=python3
 export ENVIRON=PROD
 export SRC_DIR='/home/itv000925/retail_db_data/'
-python ./python/main.py
+python ./python/app.py
