e70c92365db82fd7341857508234af04210519b9
saurabhjawa <jai_dev@macs-Mac-mini.local>
2022-02-03 17:10:05 +0530

    ultimate_second_commit

--- src/main/runme.sh
+++ src/main/runme.sh
@@ -1,5 +1,9 @@
 export HADOOP_CONF_DIR="/etc/hadoop/conf"
+export SPARK_HOME=/home/itv000925/streaming_venv/lib/python3.6/site-packages/pyspark
+export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.9-src.zip:$PYTHONPATH
+export PATH=$SPARK_HOME/bin:$SPARK_HOME/python:$PATH
 export PYSPARK_PYTHON=python3
 export ENVIRON=PROD
 export SRC_DIR='/home/itv000925/retail_db_data/'
 python ./python/app.py
+
