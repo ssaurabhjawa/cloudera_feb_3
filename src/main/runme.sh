7d008073d0fecbd28d39cd38ecdf9b3510d4ce47
saurabhjawa <jai_dev@macs-Mac-mini.local>
2022-02-03 19:33:57 +0530

    consumer-group-id_7

--- src/main/runme.sh
+++ src/main/runme.sh
@@ -7,6 +7,7 @@ export ENVIRON=PROD
 export SRC_DIR='/home/itv000925/retail_db_data/'
 spark-submit \
 --master yarn \
+--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1 \
 --deploy-mode client \
 --conf spark.ui.port=0 \
 python/app.py
